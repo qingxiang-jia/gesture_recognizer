c59d848371ab76c4559311edecac9c1c0081ec96
Qingxiang Jia <ybjqx3340@gmail.com>
2015-02-16 23:39:17 -0500

    Added comments for readVideo.m

--- f_video/readVideo.m
+++ f_video/readVideo.m
@@ -1,18 +1,18 @@
 % read the video file, and then capture frames
 video = VideoReader('f.mp4');
 filenames = cell(1, 100); % store filenames
-pos = 1;
+pos = 1; % marks last position of filename in filenames (exclusive)
 for curr=0:5:video.Duration
     video.CurrentTime = curr; % set current time to curr
     img = readFrame(video); % read the next available frame
     img = im2bw(img, 7/10); % put into binary
     filename = [sprintf('%03d', curr) '.jpg'];
-    filenames{1, pos} = filename;
-    pos = pos + 1;
-    imwrite(img, filename);
+    filenames{1, pos} = filename; % store the generated filename for future use
+    pos = pos + 1; % write the image to file
+    imwrite(img, filename); % write the image to file
 end
 
-for i=1:(pos-1)
+for i=1:(pos-1) % read a filename and load the image for recognition
     filename = filenames{i};
     recognize(filename);
 end
